92e4854f4cd64536cd020876e4cf1771b26b4391
sonph <sonph>
2022-05-06 11:12:07 +0700

    nộp bài tập

---
+++ main.js
@@ -0,0 +1,4 @@
+let a;
+a = prompt('nhập bán kính');
+area=3.14*a*a;
+document.write('diện tích hình tròn :'+ area);
---
+++ main2.js
@@ -0,0 +1,4 @@
+let d;
+d = prompt('nhậo đường kính');
+Chuvi = 3.14*d;
+document.write('chu vi hình tròn là '+ Chuvi);
